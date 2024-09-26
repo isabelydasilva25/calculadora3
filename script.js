cbe27c882d38cf8b2a01702f60c88fe79aa3f55c
isabelydasilva25 <159267254+isabelydasilva25@users.noreply.github.com>
2024-09-26 12:07:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function calcula(){
-    const vf = parseFloat(document.getElementById)
-    const taxa =
-    const np =
-    let p = (vf * Math.pot (1+ taxa, np)*taxa)/(math.pow(1+taxa,np)-1);
+function calcular() {
+    const valor = document.getElementById('valor').value;
+    const taxa = document.getElementById('taxa').value;
+    const parcela = document.getElementById('parcela').value;
+
+     const valor_parcela = (valor * Math.pow(taxa +1, parcela))/(Math.pow(taxa +1, parcela)-1);
+     const corpo_tabela  = document.querySelector('#tabela tbody');
+
+     for(let i = 1; i<=parcela; i++){
+        const linha =`
+            <tr>
+              <td>${i}</td>
+              <td>${valor_parcela.toFixed(2)}</td>
+            </tr>
+        `;
+        corpo_tabela.innerHTML += linha;
+     }
 }
